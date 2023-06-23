6b81d27109c597a24556f95004933fef4420a7b1
Ivan Malshchukov <nummyn0rih@gmail.com>
2023-06-23 21:47:58 +0300

    feat: Добавлен и подключен dev-server

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -4,4 +4,9 @@ const commonConfig = require('./webpack.config.common.js');
 module.exports = merge(commonConfig, {
   mode: 'development',
   devtool: 'inline-source-map',
+  devServer: {
+    port: 9000,
+    hot: true,
+    open: true,
+  },
 });
