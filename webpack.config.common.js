69264206a42162003ee12a2b63381b5c58116f97
Ivan Malshchukov <nummyn0rih@gmail.com>
2023-06-24 12:14:18 +0300

    feat: Добавлены шрифты

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -26,6 +26,10 @@ module.exports = {
         test: /\.css$/i,
         use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
 };
